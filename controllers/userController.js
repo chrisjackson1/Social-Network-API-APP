6b1c041f9d02526883ddb8c8e5860b6b31ea6368
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-07-10 16:53:00 -0400

    adding more edits to this app

---
+++ controllers/userController.js
@@ -0,0 +1,45 @@
+const { User, Application } = require('../models');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createUser(req, res)
+{
+    User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500) json(err));
+}
+}  
+ deleteUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId })
+    
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with this id!' })
+          : res.json(application)
+      )
+      .then(()=> res.json({message: "User and associated apps deleted"}))
+      .catch((err) => res.status(500).json(err));
+
+  },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId}, req.body)
+      .then(() => res.json({ message: "User and associated apps updated!"}))
+      .catch ((err ) => res.status(500).json(err));
+};
